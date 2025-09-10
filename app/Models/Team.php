bfb00f11519bbbfdcd3b2a98ea03bd0db8b69474
Henry Lee <henryleeworld@gmail.com>
2025-09-11 00:01:44 +0800

    feat:升級至 laravel 12

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -10,7 +10,7 @@ class Team extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
